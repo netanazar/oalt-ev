511ff5325b0db413cd287374c7200b276fd28955
opchandra <opchandraofficial@gmail.com>
2026-02-25 05:01:35 +0530

    Initial Django project

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *  # noqa: F403,F401
 
-DEBUG = True
+DEBUG = False
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
@@ -12,4 +12,13 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
-ALLOWED_HOSTS = ['*']    
+ALLOWED_HOSTS = ['oalt-ev.onrender.com', 'localhost', '127.0.0.1']
+
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
